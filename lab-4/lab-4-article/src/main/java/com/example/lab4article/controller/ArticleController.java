6e4675c20493ecec72d56053d1b6ac8637257454
Marco Antonio MVRD. Alvarado Arteaga <marco.alvarado@SofttekClients.com>
2017-09-29 18:16:26 -0500

    Lab 4 complete. Eureka with Config.

---
+++ lab-4/lab-4-article/src/main/java/com/example/lab4article/controller/ArticleController.java
@@ -0,0 +1,24 @@
+package com.example.lab4article.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Marco Alvarado <marco.alvarado@softtek.com>
+ * @date Sep 28, 2017
+ */
+@RestController
+public class ArticleController
+{
+    @Value("${words}")
+    private String words;
+
+    @GetMapping("/")
+    public @ResponseBody String getWord() {
+        String[] wordArray = words.split(",");
+        int i = (int) Math.round(Math.random() * (wordArray.length - 1));
+        return wordArray[i];
+    }
+}
